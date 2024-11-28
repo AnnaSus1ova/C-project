4c1be9a8a56e58d60304e3a8623eee480ab83074
Anna Suslova <suslova.as@phystech.edu>
2024-11-29 00:08:39 +0300

    Add relative path to background image

--- view/mainwindow.cpp
+++ view/mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix(":/images/img/my-background.png");
+
+    ui->picture->setPixmap(pix);
 }
 
 MainWindow::~MainWindow()
