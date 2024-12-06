0a9ff8782f65971ae0c6d338d77a9bde84fdbd31
Alesialoco <125814881+Alesialoco@users.noreply.github.com>
2024-12-06 22:09:07 +0300

    Add setters to the class

--- Vector.cpp
+++ Vector.cpp
@@ -2,6 +2,7 @@
 #include "Vector.h"
 #include <vector>
 
+Vector_2d::Vector_2d(){}
 
 Vector_2d::Vector_2d(std::vector<double> _abscissa, std::vector<double> _ordinate){
     try{
@@ -35,4 +36,12 @@ std::vector<double> Vector_2d::get_abscissa(){
 
 std::vector<double> Vector_2d::get_ordinate(){
     return ordinate;
+}
+
+void Vector_2d::set_abscissa(std::vector<double> _abscis){
+    abscissa = _abscis;
+}
+
+void Vector_2d::set_ordinate(std::vector<double> _ordin){
+    ordinate = _ordin;
 }
